3c6da878450317b38be17dedf526c01dc0d51e0e
Javier Preciado <jpreciado85@gmail.com>
2024-07-04 16:04:28 -0500

    creación de rutas de prueba

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function Pest\Laravel\patch;
+
+//blog.test/
 Route::get('/', function () {
-    return view('welcome');
+    return "Hola, este mensaje aparecerá como entrada a la app";
 });
+
+//blog.test/categoria
+Route::get('/categoria', function() {
+    return "bienvenido a la categoría";
+});
+
+//blog.test/categoria/#######
+Route::get('/categoria/{subcategoria}', function($subcategoria) {
+    return "aquí verás la subcategoría de {$subcategoria} dentro de la categoría";
+});
+
+//blog.test/######
+Route::get('/{categoría}', function($categoria){
+    return "bienvenido a la categoria de {$categoria}";
+});
+
+//blog.test/######/######
+Route::get('/{categoria}/{subcategoria}', function($categoria, $subcategoria) {
+    return "te has dirigido a la categoria de {$categoria} y te encuentras en {$subcategoria}";
+});
+
+
+/* TIPOS DE PETICIONES */
+
+//Get
+//Post
+//Put
+//Patch
+//Delete
+
